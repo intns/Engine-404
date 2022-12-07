4cedca91716b8ac4d2f2ae225ed0ccd93c2af525
intns <84647527+intns@users.noreply.github.com>
2022-12-07 01:38:59 +0000

    BUGFIX: Whistle line clipping no longer occurs

--- Assets/Scripts/_Player/Player.cs
+++ Assets/Scripts/_Player/Player.cs
@@ -44,158 +44,167 @@ public class Player : MonoBehaviour, IHealth
 	}
 
 	[SerializeField] AnimationClip _WalkAnimation;
-[SerializeField] AnimationClip _DieAnimation;
-[SerializeField] AnimationClip _DamageAnimation;
-[SerializeField] AnimationClip _Idle1Animation;
-[SerializeField] AnimationClip _Idle2Animation;
+	[SerializeField] AnimationClip _DieAnimation;
+	[SerializeField] AnimationClip _DamageAnimation;
+	[SerializeField] AnimationClip _Idle1Animation;
+	[SerializeField] AnimationClip _Idle2Animation;
 
-[SerializeField] private CharacterController _Controller;
-bool _IsHit = false;
-bool _Walking = false;
+	[SerializeField] private CharacterController _Controller;
+	bool _IsHit = false;
+	bool _Walking = false;
 
-private void OnEnable()
-{
-	_Instance = this;
-}
-
-private void Awake()
-{
-	_MovementController = GetComponent<PlayerMovementController>();
-	_PikminController = GetComponent<PlayerPikminController>();
-
-	Debug.Assert(PlayerAnimation.Walk == _AnimController.AddState(_WalkAnimation));
-	Debug.Assert(PlayerAnimation.Die == _AnimController.AddState(_DieAnimation));
-	Debug.Assert(PlayerAnimation.Damage == _AnimController.AddState(_DamageAnimation));
-	Debug.Assert(PlayerAnimation.Idle1 == _AnimController.AddState(_Idle1Animation));
-	Debug.Assert(PlayerAnimation.Idle2 == _AnimController.AddState(_Idle2Animation));
-
-	// Resets the health back to the max if changed in the editor
-	_CurrentHealth = _MaxHealth;
-}
-
-private void Update()
-{
-	if (!GameManager.IsPaused)
+	private void OnEnable()
 	{
-		if (_Walking)
+		_Instance = this;
+	}
+
+	private void Awake()
+	{
+		_MovementController = GetComponent<PlayerMovementController>();
+		_PikminController = GetComponent<PlayerPikminController>();
+
+		Debug.Assert(PlayerAnimation.Walk == _AnimController.AddState(_WalkAnimation));
+		Debug.Assert(PlayerAnimation.Die == _AnimController.AddState(_DieAnimation));
+		Debug.Assert(PlayerAnimation.Damage == _AnimController.AddState(_DamageAnimation));
+		Debug.Assert(PlayerAnimation.Idle1 == _AnimController.AddState(_Idle1Animation));
+		Debug.Assert(PlayerAnimation.Idle2 == _AnimController.AddState(_Idle2Animation));
+
+		// Resets the health back to the max if changed in the editor
+		_CurrentHealth = _MaxHealth;
+	}
+
+	private void Update()
+	{
+		if (!GameManager.IsPaused)
 		{
-			_AnimController.ChangeState(PlayerAnimation.Walk);
-		}
-		else if (_IsHit)
-		{
-			_AnimController.ChangeState(PlayerAnimation.Damage, true);
-			_IsHit = false;
+			if (_Walking)
+			{
+				_AnimController.ChangeState(PlayerAnimation.Walk);
+			}
+			else if (_IsHit)
+			{
+				_AnimController.ChangeState(PlayerAnimation.Damage, true);
+				_IsHit = false;
+			}
+			else
+			{
+				_AnimController.ChangeState(PlayerAnimation.Idle, true);
+			}
+
+			// Handle health-related functions
+			if (_CurrentHealth <= 0)
+			{
+				Die();
+			}
+			
+			// BUGFIX: Whistle line clips backwards and all sorts if you don't check the length
+			if (MathUtil.DistanceTo(transform.position, _WhistleController._Reticle.position) < 5)
+			{
+				_WhistleLine.SetPosition(0, Vector3.zero);
+				_WhistleLine.SetPosition(1, Vector3.zero);
+			}
+			else
+			{
+				// Offset the whistle line from the player position and then the reticle position
+				Vector3 dir1 = MathUtil.DirectionFromTo(transform.position, _WhistleController._Reticle.position, true);
+				Vector3 dir2 = MathUtil.DirectionFromTo(_WhistleController._Reticle.position, transform.position, true);
+
+				_WhistleLine.SetPosition(0, transform.position + dir1);
+				_WhistleLine.SetPosition(1, _WhistleController._Reticle.position + dir2);
+			}
 		}
 		else
 		{
+			_WhistleLine.SetPosition(0, Vector3.zero);
+			_WhistleLine.SetPosition(1, Vector3.zero);
+
 			_AnimController.ChangeState(PlayerAnimation.Idle, true);
 		}
+	}
 
-		// Handle health-related functions
-		if (_CurrentHealth <= 0)
+	public void Die()
+	{
+		DayTimeManager._Instance.FinishDay();
+	}
+
+	public void PikminExtinction()
+	{
+		StartCoroutine(IE_PikminExtinctionSequence());
+	}
+
+	public void Pause(PauseType type)
+	{
+		_AnimController.ChangeState(PlayerAnimation.Idle);
+
+		_MovementController._Paralysed = type != PauseType.Unpaused;
+		GameManager.PauseType = type;
+	}
+
+	// When start key is pressed
+	public void OnStart()
+	{
+		Die();
+	}
+
+	// Happens whenever the movement joystick/buttons change values
+	public void OnMovement(InputAction.CallbackContext context)
+	{
+		// If NOT paralyzed AND NOT paused AND IS moving any direction
+		_Walking = (!_MovementController._Paralysed) && (!GameManager.IsPaused) && ((context.ReadValue<Vector2>().x != 0) || (context.ReadValue<Vector2>().y != 0));
+	}
+
+	#region Animation Callbacks
+
+	#endregion
+
+	#region Health Implementation
+
+	// 'Getter' functions
+	public float GetCurrentHealth()
+	{
+		return _CurrentHealth;
+	}
+
+	public float GetMaxHealth()
+	{
+		return _MaxHealth;
+	}
+
+	// 'Setter' functions
+	public float AddHealth(float give)
+	{
+		return _CurrentHealth += give;
+	}
+
+	public float SubtractHealth(float take)
+	{
+		_IsHit = true;
+		_AnimController.ChangeState(PlayerAnimation.Damage, true);
+		return _CurrentHealth -= take;
+	}
+
+	public void SetHealth(float set)
+	{
+		if (_CurrentHealth > set)
 		{
-			Die();
+			_AnimController.ChangeState(PlayerAnimation.Damage, true);
+			_IsHit = true;
 		}
 
-		// Offset the whistle line from the player position and then the reticle position
-		Vector3 dir1 = MathUtil.DirectionFromTo(transform.position, _WhistleController._Reticle.position, true);
-		Vector3 dir2 = MathUtil.DirectionFromTo(_WhistleController._Reticle.position, transform.position, true);
-
-		_WhistleLine.SetPosition(0, transform.position + dir1);
-		_WhistleLine.SetPosition(1, _WhistleController._Reticle.position + dir2);
+		_CurrentHealth = set;
 	}
-	else
+
+	#endregion
+
+	#region IEnumerators
+	IEnumerator IE_PikminExtinctionSequence()
 	{
-		_WhistleLine.SetPosition(0, Vector3.zero);
-		_WhistleLine.SetPosition(1, Vector3.zero);
+		Pause(PauseType.Paused);
+		_UIController.FadeOutUI();
 
-		_AnimController.ChangeState(PlayerAnimation.Idle, true);
+		yield return new WaitForSecondsRealtime(3.5f);
+
+		Die();
 	}
-}
-
-public void Die()
-{
-	DayTimeManager._Instance.FinishDay();
-}
-
-public void PikminExtinction()
-{
-	StartCoroutine(IE_PikminExtinctionSequence());
-}
-
-public void Pause(PauseType type)
-{
-	_AnimController.ChangeState(PlayerAnimation.Idle);
-
-	_MovementController._Paralysed = type != PauseType.Unpaused;
-	GameManager.PauseType = type;
-}
-
-// When start key is pressed
-public void OnStart()
-{
-	Die();
-}
-
-// Happens whenever the movement joystick/buttons change values
-public void OnMovement(InputAction.CallbackContext context)
-{
-	// If NOT paralyzed AND NOT paused AND IS moving any direction
-	_Walking = (!_MovementController._Paralysed) && (!GameManager.IsPaused) && ((context.ReadValue<Vector2>().x != 0) || (context.ReadValue<Vector2>().y != 0));
-}
-
-#region Animation Callbacks
-
-#endregion
-
-#region Health Implementation
-
-// 'Getter' functions
-public float GetCurrentHealth()
-{
-	return _CurrentHealth;
-}
-
-public float GetMaxHealth()
-{
-	return _MaxHealth;
-}
-
-// 'Setter' functions
-public float AddHealth(float give)
-{
-	return _CurrentHealth += give;
-}
-
-public float SubtractHealth(float take)
-{
-	_IsHit = true;
-	_AnimController.ChangeState(PlayerAnimation.Damage, true);
-	return _CurrentHealth -= take;
-}
-
-public void SetHealth(float set)
-{
-	if (_CurrentHealth > set)
-	{
-		_AnimController.ChangeState(PlayerAnimation.Damage, true);
-		_IsHit = true;
-	}
-
-	_CurrentHealth = set;
-}
-
-#endregion
-
-#region IEnumerators
-IEnumerator IE_PikminExtinctionSequence()
-{
-	Pause(PauseType.Paused);
-	_UIController.FadeOutUI();
-
-	yield return new WaitForSecondsRealtime(3.5f);
-
-	Die();
-}
 	#endregion
 }
