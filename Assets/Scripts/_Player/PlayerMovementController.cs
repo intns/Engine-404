740c1cfd96e29352c268c9148dcb641fd3c876f6
intns <84647527+intns@users.noreply.github.com>
2023-05-16 03:05:51 +0100

    Redux of player movement controller, sliding!

--- Assets/Scripts/_Player/PlayerMovementController.cs
+++ Assets/Scripts/_Player/PlayerMovementController.cs
@@ -1,14 +1,6 @@
-/*
- * PlayerMovementController.cs
- * Created by: Ambrosia
- * Created on: 7/2/2020 (dd/mm/yy)
- * Last update by : Senka
- * Last update on : 9/7/2022
- * Created for: controlling the movement of the Player
- */
-
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovementController : MonoBehaviour
 {
@@ -19,34 +11,41 @@ public class PlayerMovementController : MonoBehaviour
 
 	[Header("Settings")]
 	[SerializeField] float _MovementSpeed = 3;
-	[SerializeField] Vector2 _MovementDeadzone = Vector2.one / 0.1f;
-	[SerializeField] float _SlideLimit = 25;
-	[SerializeField] float _SlideSpeed = 5;
 	[SerializeField] float _RotationSpeed = 3;
-	float _Gravity = Physics.gravity.y;
-	[SerializeField] float _Weight = 0.01f;
+	[SerializeField] float _MovementDeadzone = 0.1f;
 
+	[SerializeField, Tooltip("The time the player has to be idle before looking at the whistle.")]
+	float _LookAtWhistleTime = 2.5f;
+
+	[SerializeField] float _Weight = 0.01f;
 	[SerializeField] LayerMask _MapMask;
 
-	[SerializeField] float _LookAtWhistleTime = 2.5f;
+	[Header("Sliding Settings")]
+	[SerializeField, Tooltip("The speed at which the player slides.")]
+	float _SlideSpeed = 2;
+
+	[SerializeField, Tooltip("The maximum speed at which the player can slide.")]
+	float _MaxSlideSpeed = 10;
+
+	[SerializeField, Range(1.0f, 180.0f), Tooltip("The minimum slope angle at which the player can slide.")]
+	float _MinSlopeAngle = 25;
+
+	[SerializeField, Range(0.01f, 1.0f), Tooltip("The friction applied to the player when sliding.")]
+	float _SlideFriction = 0.15f;
 
 	[HideInInspector] public Quaternion _RotationBeforeIdle = Quaternion.identity;
 	[HideInInspector] public bool _Paralysed = false;
 
+	Vector3 _SlideDirection = Vector3.zero;
+	bool _IsSliding = false;
+
 	float _IdleTimer = 0;
 	Vector3 _Movement;
-	RaycastHit _SlideHit;
-	Vector3 _CharacterContactPoint;
 	Vector3 _HitNormal;
-	float _SlideRayDist;
 
 	void Awake()
 	{
 		_Controller = GetComponent<CharacterController>();
-		// _Controller.detectCollisions = false;
-
-		_SlideRayDist = (_Controller.height * .5f) + _Controller.radius;
-		_SlideLimit = _Controller.slopeLimit - .1f;
 		_MainCamera = Camera.main;
 	}
 
@@ -57,6 +56,81 @@ public class PlayerMovementController : MonoBehaviour
 			return;
 		}
 
+		UpdateLookAtWhistle();
+		ApplyGravity();
+		ApplySlide();
+		UpdateMovement();
+	}
+
+	/// <summary>
+	/// Updates player movement based on input. If no Pikmin are being held, the player rotates towards the movement direction.
+	/// </summary>
+	void UpdateMovement()
+	{
+		Vector3 movementDirection = _Movement;
+		movementDirection.y = 0f;
+
+		// If the player has not moved enough, return
+		if (movementDirection.magnitude <= _MovementDeadzone)
+		{
+			return;
+		}
+
+		if (_IsSliding)
+		{
+			movementDirection -= Vector3.Project(movementDirection, _SlideDirection);
+		}
+
+		// We've moved, so the idle timer gets reset
+		_IdleTimer = 0;
+
+		movementDirection = _MainCamera.transform.TransformDirection(movementDirection);
+		movementDirection.y = 0f;
+
+		// Only rotate if there isn't a Pikmin in hand, as it has priority
+		if (Player._Instance._PikminController._PikminInHand == null)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), _RotationSpeed * Time.deltaTime);
+		}
+
+		// Move the controller
+		_Controller.Move(_MovementSpeed * Time.deltaTime * movementDirection);
+	}
+
+	/// <summary>
+	/// Applies sliding effect on slopes by adjusting movement using slide direction, speed, and friction.
+	/// </summary>
+	void ApplySlide()
+	{
+		if (!_Controller.isGrounded || Vector3.Angle(_HitNormal, Vector3.up) <= _MinSlopeAngle)
+		{
+			_IsSliding = false;
+			return;
+		}
+
+		_IsSliding = true;
+
+		// Calculate slide direction
+		_SlideDirection = Vector3.ProjectOnPlane(Vector3.down, _HitNormal).normalized;
+
+		// Calculate slide speed and friction
+		float slopeAngle = Vector3.Angle(_HitNormal, Vector3.up);
+		float slideSpeedFactor = Mathf.InverseLerp(_MinSlopeAngle, _Controller.slopeLimit, slopeAngle);
+		float slideSpeed = Mathf.Lerp(_SlideSpeed, _MaxSlideSpeed, slideSpeedFactor);
+		Vector3 slideMovement = _SlideDirection * slideSpeed;
+
+		// Apply friction to reduce slide speed over time
+		slideMovement *= Mathf.Clamp01(1f / (1f + _SlideFriction * Time.deltaTime));
+
+		// Apply slide movement
+		_Controller.Move(slideMovement * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// Smoothly rotates the character towards the whistle's position if enough idle time has passed.
+	/// </summary>
+	void UpdateLookAtWhistle()
+	{
 		// Add time to the idle timer
 		_IdleTimer += Time.deltaTime;
 
@@ -73,90 +147,35 @@ public class PlayerMovementController : MonoBehaviour
 		{
 			_RotationBeforeIdle = transform.rotation;
 		}
+	}
 
+	/// <summary>
+	/// Applies gravity to a character controller, adjusting its vertical movement based on weight and grounded state.
+	/// </summary>
+	void ApplyGravity()
+	{
 		_Controller.Move(new Vector3(0, _Movement.y, 0));
-		// If we're not grounded and not on a slope
-		if (!_Controller.isGrounded)
+
+		float gravityForce = Physics.gravity.y * _Weight * Time.deltaTime;
+		if (_Controller.isGrounded)
 		{
-			// Apply gravity
-			_Movement.y += _Gravity * _Weight * Time.deltaTime;
+			_Movement.y = gravityForce;
 		}
 		else
 		{
-			// Else, stop downward movement (but keep pushing it a bit toward the ground because
-			// characterControllers are buggy)
-			_Movement.y = _Gravity * _Weight * Time.deltaTime;
+			_Movement.y += gravityForce;
 		}
-
-		// Get current movement and make it a Vector3
-		Vector3 mDirection = _Movement;
-		mDirection.y = 0;
-
-		// If the player has even touched the H and V axis
-		if (Mathf.Abs(mDirection.x) <= _MovementDeadzone.x && Mathf.Abs(mDirection.z) <= _MovementDeadzone.y)
-		{
-			return;
-		}
-
-		// We've moved, so the idle timer gets reset
-		_IdleTimer = 0;
-
-		// Make the movement vector relative to the camera's position/rotation
-		// and remove any Y momentum gained from doing the TransformDirection
-		mDirection = _MainCamera.transform.TransformDirection(mDirection);
-		mDirection.y = 0;
-
-		// Only rotate if there isn't a Pikmin in hand, as it has priority
-		if (Player._Instance._PikminController._PikminInHand == null)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mDirection), _RotationSpeed * Time.deltaTime);
-		}
-
-		Vector3 movement = mDirection.normalized * _MovementSpeed;
-
-		bool sliding = false;
-		// See if surface immediately below should be slid down. We use this normally rather than a ControllerColliderHit point,
-		// because that interferes with step climbing amongst other annoyances
-		if (Physics.SphereCast(transform.position, 0.75f, Vector3.down, out _SlideHit, _SlideRayDist, _MapMask, QueryTriggerInteraction.Ignore)
-			&& Vector3.Angle(_SlideHit.normal, Vector3.up) > _SlideLimit)
-		{
-			sliding = true;
-		}
-		// However, just raycasting straight down from the center can fail when on steep slopes
-		// So if the above raycast didn't catch anything, raycast down from the stored ControllerColliderHit point instead
-		else
-		{
-			if (Physics.Raycast(_CharacterContactPoint + Vector3.up, -Vector3.up, out _SlideHit, _SlideRayDist, _MapMask, QueryTriggerInteraction.Ignore)
-				&& Vector3.Angle(_SlideHit.normal, Vector3.up) > _SlideLimit)
-			{
-				sliding = true;
-			}
-		}
-
-		if (sliding)
-		{
-			movement.x += (1f - _SlideHit.normal.y) * _SlideHit.normal.x * _SlideSpeed;
-			movement.z += (1f - _SlideHit.normal.y) * _SlideHit.normal.z * _SlideSpeed;
-		}
-		else if (Vector3.Angle(Vector3.up, _HitNormal) > _Controller.slopeLimit)
-		{
-			movement.x += (1f - _HitNormal.y) * _HitNormal.x * _SlideSpeed;
-			movement.z += (1f - _HitNormal.y) * _HitNormal.z * _SlideSpeed;
-		}
-
-		_Controller.Move(movement * Time.deltaTime);
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit hit)
 	{
-		_CharacterContactPoint = hit.point;
 		_HitNormal = hit.normal;
 	}
 
 	// Happens whenever the movement joystick/buttons change values
 	public void OnMovement(InputAction.CallbackContext context)
 	{
-		// Stores the current movement (already normalized)
+		// The movement directions are already normalized
 		_Movement = new Vector3(context.ReadValue<Vector2>().x, _Movement.y, context.ReadValue<Vector2>().y);
 	}
 }
